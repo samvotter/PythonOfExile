f4bf75bc0b948cbb7c6a86ab83031a7abb614580
svanotte <sam.van.otterloo@intel.com>
2020-12-21 10:47:26 -0800

    Added Attribute Keyword. Slight refactor to incorporate new class

--- Characters/Atttributes/attribute.py
+++ Characters/Atttributes/attribute.py
@@ -1,55 +1,198 @@
 from numpy import prod
 
 
+class AttributeKeyword:
+    """
+    Description:
+        Assists tracking and calculating values associated with Attributes
+
+    Example:
+        Strength
+        Dexterity
+        Intelligence
+
+        Additional
+        Increase
+        Decrease
+        More
+        Less
+    """
+    def __init__(self, sources=None):
+        self.sources = {}.update(sources)
+        self.current = 0
+
+    def update(self):
+        return self.current
+
+
+class LevelModifier(AttributeKeyword):
+    """
+    Description:
+        How much either Strength, Dexterity, or Intelligence contributes to an attribute
+
+    Example:
+        Strength                = 50
+        Contribution to life    = (5/10)
+        Total                   = 25
+    """
+    def __init__(self, sources=None):
+        super().__init__(sources=sources)
+        self.current = sum(self.sources.values())
+
+    def update(self):
+        self.current = sum(self.sources.values())
+        return self.current
+
+
+class SDIModifier(AttributeKeyword):
+    """
+    Description:
+        How much either Strength, Dexterity, or Intelligence contributes to an attribute
+
+    Example:
+        Strength                = 50
+        Contribution to life    = (5/10)
+        Total                   = 25
+    """
+    def __init__(self, sources=None):
+        super().__init__(sources=sources)
+        self.current = sum(self.sources.values())
+
+    def update(self):
+        self.current = sum(self.sources.values())
+        return self.current
+
+
+class Additional(AttributeKeyword):
+    """
+    Description:
+        Additional refers to a flat bonus to the underlying base value.
+
+    Example:
+        Base Damage         = 50
+        Additional Damage   = 30
+        Total Damage        = 80
+    """
+    def __init__(self, sources=None):
+        super().__init__(sources=sources)
+        self.current = sum(self.sources.values())
+
+    def update(self):
+        self.current = sum(self.sources.values())
+        return self.current
+
+
+class Increase(AttributeKeyword):
+    """
+    Description:
+        Additional refers to a percentage bonus to the underlying base value.
+
+    Example:
+        Base Damage         = 50
+        Increase Damage     = 30
+        Total Damage        = 65
+    """
+
+    def __init__(self, sources=None):
+        super().__init__(sources=sources)
+        self.current = sum([val/100 for val in self.sources.values()])
+
+    def update(self):
+        self.current = sum([val/100 for val in self.sources.values()])
+        return self.current
+
+
+class Decrease(AttributeKeyword):
+    """
+    Description:
+        Decrease refers to a percentage penalty to the underlying base value.
+
+    Example:
+        Base Damage         = 50
+        Decrease Damage     = 30
+        Total Damage        = 35
+    """
+
+    def __init__(self, sources=None):
+        super().__init__(sources=sources)
+        self.current = -sum([val/100 for val in self.sources.values()])
+
+    def update(self):
+        self.current = -sum([val/100 for val in self.sources.values()])
+        return self.current
+
+
+class More(AttributeKeyword):
+    """
+    Description:
+        More refers to a cumulative percentage bonus to the total value.
+
+    Example:
+        Base Damage         = 50
+        Decrease Damage     = 10, 10, 10
+        Total Damage        = 66.55
+    """
+
+    def __init__(self, sources=None):
+        super().__init__(sources=sources)
+        self.current = prod([1 + val/100 for val in self.sources.values()])
+
+    def update(self):
+        self.current = prod([1 + val/100 for val in self.sources.values()])
+        return self.current
+
+
+class Less(AttributeKeyword):
+    """
+    Description:
+        Less refers to a cumulative percentage penalty to the total value.
+
+    Example:
+        Base Damage         = 50
+        Decrease Damage     = 10, 10, 10
+        Total Damage        = 36.45
+    """
+
+    def __init__(self, sources=None):
+        super().__init__(sources=sources)
+        self.current = prod([1 - val/100 for val in self.sources.values()])
+
+    def update(self):
+        self.current = prod([1 - val/100 for val in self.sources.values()])
+        return self.current
+
+
 class Attribute:
 
     def __init__(
             self,
             base=0,
-            per_level=0,
+            level_modifier=None,
             strength_modifier=0.0,
             dexterity_modifier=0.0,
-            intelligence_modifier=0.0
+            intelligence_modifier=0.0,
+            add_mods=None,
+            inc_mods=None,
+            dec_mods=None,
+            more_mods=None,
+            less_mods=None
     ):
-        self._base = base
-        self._per_level = per_level
-        self._attribute_modifiers = {
-            "strength": strength_modifier,
-            "dexterity": dexterity_modifier,
-            "intelligence": intelligence_modifier
+        self._base                  = base
+        self._per_level             = LevelModifier(sources=level_modifier)
+        self._sdi_modifiers   = {
+            "strength":     SDIModifier(sources={"base": strength_modifier}),
+            "dexterity":    SDIModifier(sources={"base": dexterity_modifier}),
+            "intelligence": SDIModifier(sources={"base": intelligence_modifier}),
         }
+        self._additional            = Additional(sources=add_mods)
+        self._increase              = Increase(sources=inc_mods)
+        self._decrease              = Decrease(sources=dec_mods)
+        self._more                  = More(sources=more_mods)
+        self._less                  = Less(sources=less_mods)
 
-    def get_total(
-            self,
-            character_level=1,
-            sdi=None,
-            additional=None,
-            increase=None,
-            decrease=None,
-            more=None,
-            less=None
-    ):
-        attr_contribution = 0
-        add_contribution = 0
-        inc_contribution = 0
-        dec_contribution = 0
-        more_contribution = 1
-        less_contribution = 1
-        if sdi:
-            attr_contribution = sum([self._attribute_modifiers[key] * sdi[key] for key in sdi.keys()])
-        if additional:
-            add_contribution = sum(additional)
-        if increase:
-            inc_contribution = sum([val/100 for val in increase])
-        if decrease:
-            dec_contribution = sum([val/100 for val in decrease])
-        if more:
-            more_contribution = prod([1 + val/100 for val in more])
-        if less:
-            less_contribution = prod([1 - val/100 for val in less])
-        flat = self._base + (character_level * self._per_level) + attr_contribution + add_contribution
-        inc_or_dec = 1 + inc_contribution - dec_contribution
-        return int(flat * inc_or_dec * more_contribution * less_contribution)
+    def get_total(self, level=0):
+        base = self._base + (level * self._per_level.current) + self._additional.current
+        base += sum([val.current for val in self._sdi_modifiers.values()])
 
 
 class Strength(Attribute):
